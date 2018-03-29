78105a29b1cd2854ae01ec00b6a574c758d46934
nishisuke <nishisuke13+git@gmail.com>
2018-03-29 13:07:39 +0900

    refact redcarpet

--- redcarpet_wrapper.rb
+++ redcarpet_wrapper.rb
@@ -2,21 +2,21 @@ require './wrapper'
 require 'redcarpet'
 
 class RedcarpetWrapper < Wrapper
+  class TaskListRender < Redcarpet::Render::HTML
+    def list_item(text, list_type)
+      if m = text.match(/\A\[[x ]?\] /)
+        "<li><input type='checkbox'>#{m.post_match}</li>"
+      else
+        "<li>#{text}</li>"
+      end
+    end
+  end
+
   def initialize
     @renderer = Redcarpet::Markdown.new(TaskListRender)
   end
 
   def render_html(markdown)
     renderer.render(markdown)
-  end
-end
-
-class TaskListRender < Redcarpet::Render::HTML
-  def list_item(text, list_type)
-    if m = text.match(/\A\[[x ]?\] /)
-      "<li><input type='checkbox'>#{m.post_match}</li>"
-    else
-      "<li>#{text}</li>"
-    end
   end
 end
