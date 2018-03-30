32c76ce53d266c89bb97ef68383fe9b13577ba4b
nishisuke <nishisuke13+git@gmail.com>
2018-03-30 16:09:49 +0900

    refact commonmaker implment

--- commonmarker_wrapper.rb
+++ commonmarker_wrapper.rb
@@ -18,7 +18,8 @@ class CommonmarkerWrapper < Wrapper
     end
 
     def list_item(node)
-      check_box_tag = if node.first_child.first_child.string_content.match(/\A\[[x ]?\] /)
+      check_box_tag = if m = node.first_child.first_child.string_content.match(/\A\[[x ]?\] /)
+        node.first_child.first_child.string_content = m.post_match
         @task_list_item_id += 1
         "<input type='checkbox' data-pos=#{sourcepos(node)} data-task-key=#{@task_list_item_id}>"
       end
@@ -28,14 +29,6 @@ class CommonmarkerWrapper < Wrapper
           out(:children)
         end
       end
-    end
-
-    def text(node)
-      if node.parent.parent.type === :list_item && m = node.string_content.match(/\A\[[x ]?\] /)
-        node.string_content = m.post_match
-      end
-
-      out(escape_html(node.string_content))
     end
   end
 end
